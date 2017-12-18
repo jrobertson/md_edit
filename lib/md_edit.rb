fb2b62259d6609e952c6d5d039d269e900bda253
Your Name <you@example.com>
2017-12-18 12:56:08 +0000

    feature: A section can now be edited and saved to file.

--- lib/md_edit.rb
+++ lib/md_edit.rb
@@ -9,22 +9,35 @@ require 'phrase_lookup'
 
 class MdEdit
 
+  attr_reader :sections
+  
   # pass in an MD document
   #
-  def initialize(s=nil)
+  def initialize(md)
     
-    if s then
-      
-      parse s
-
-      @h =  @sections.keys.inject({}) do |r,x|
-        r.merge(x.sub(/^#+ +/,'').downcase => 5 - x.count('#'))
-      end      
-      
-      @pl = PhraseLookup.new @h
-      
+    s, @filename = if md.lines.length == 1 and File.exists? md then
+      [File.read(md), md]
+    else
+      md
     end
+    
+    load_sections(s)
+    
   end
+  
+  def update(heading, value)
+
+    key = @sections.keys.grep(/#{heading.downcase}/i).first
+
+    old_value = @sections[key].flatten.join("\n\n")
+    @s.sub!(key + "\n\n" + old_value, value)
+    load_sections(@s)
+    
+    save()
+    
+  end
+  
+  alias edit update
 
   def find(s)
     key = @sections.keys.grep(/#{s.downcase}/i).first
@@ -39,9 +52,29 @@ class MdEdit
   
   def to_h()
     @h
-  end  
+  end
+
+  def to_s()
+    @s
+  end
   
   private
+  
+  def load_sections(raw_s)
+    
+    # strip out any new lines gaps which are greater than 1    
+    s = raw_s.strip.gsub(/\n\s*\n\s*\n\s*/,"\n\n")
+    
+    @sections = parse s
+
+    @h =  @sections.keys.inject({}) do |r,x|
+      r.merge(x.sub(/^#+ +/,'').downcase => 5 - x.count('#'))
+    end      
+    
+    @pl = PhraseLookup.new @h        
+    @s = s
+    
+  end
     
   def parse(s)
 
@@ -60,10 +93,14 @@ class MdEdit
     a3 = LineTree.new(a2.join).to_a
 
     h = {}
-
     a4 = scan a3, h
-    @sections = h  
     
+    return h  
+    
+  end
+  
+  def save()
+    File.write @filename, @s if @filename
   end
  
   def scan(a, h={})
